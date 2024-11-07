3a1ebd5679a50f81863db3d0a8cc5f26032f8033
roshan <roshan@neokred.tech>
2024-11-07 12:22:33 +0530

    feat: create, pay api added with confirm handler

--- components/Payment.tsx
+++ components/Payment.tsx
@@ -4,6 +4,8 @@ import { PaymentSheetError, useStripe } from "@stripe/stripe-react-native";
 import { Button } from "react-native";
 import { useEffect, useState } from "react";
 import { PaymentProps } from "@/types/type";
+import { useLocationsStore } from "@/store";
+import { fetchAPI } from "@/lib/fetch";
 
 const Payment = ({
   fullName,
@@ -13,15 +15,33 @@ const Payment = ({
   rideTime,
 }: PaymentProps) => {
   const { initPaymentSheet, presentPaymentSheet } = useStripe();
-  const [success, setSuccess] = useState<boolean>(false);
-  const openPaymentSheet = async () => {
-    await initializePaymentSheet();
-    const { error } = await presentPaymentSheet();
+  const {
+    userAddress,
+    userLongitude,
+    userLatitude,
+    destinationLatitude,
+    destinationAddress,
+    destinationLongitude,
+  } = useLocationsStore();
 
-    if (error) {
-      Alert.alert(`Error code: ${error.code}`, error.message);
-    } else {
-      setSuccess(true);
+  const [success, setSuccess] = useState<boolean>(false);
+
+  const openPaymentSheet = async () => {
+    try {
+      await initializePaymentSheet();
+      const { error } = await presentPaymentSheet();
+      if (error) {
+        Alert.alert(`Error code: ${error.code}`, error.message);
+        console.error("Error presenting payment sheet:", error);
+      } else {
+        setSuccess(true);
+      }
+    } catch (err) {
+      Alert.alert(
+        "Payment Error",
+        "Something went wrong while opening the payment sheet."
+      );
+      console.error("Error in openPaymentSheet:", err);
     }
   };
 
@@ -31,31 +51,82 @@ const Payment = ({
       intentConfiguration: {
         mode: {
           amount: parseInt(amount) * 100,
-          currencyCode: "USD",
+          currencyCode: "usd",
         },
-        confirmHandler: confirmHandler,
+        confirmHandler: async (
+          paymentMethod,
+          shouldSavePaymentMethod,
+          intentCreationCallback
+        ) => {
+          const { paymentIntent, customer } = await fetchAPI(
+            "/(api)/(stripe)/create",
+            {
+              method: "POST",
+              headers: {
+                "Content-Type": "application/json",
+              },
+              body: JSON.stringify({
+                name: fullName || email.split("@")[0],
+                email: email,
+                amount: amount,
+                paymentMethodId: paymentMethod.id,
+              }),
+            }
+          );
+
+          if (paymentIntent.client_secret) {
+            const { result } = await fetchAPI("/(api)/(stripe)/pay", {
+              method: "POST",
+              headers: {
+                "Content-Type": "application/json",
+              },
+              body: JSON.stringify({
+                payment_method_id: paymentMethod.id,
+                payment_intent_id: paymentIntent.id,
+                customer_id: customer,
+                client_secret: paymentIntent.client_secret,
+              }),
+            });
+
+            if (result.client_secret) {
+              await fetchAPI("/(api)/ride/create", {
+                method: "POST",
+                headers: {
+                  "Content-Type": "application/json",
+                },
+                body: JSON.stringify({
+                  origin_address: userAddress,
+                  destination_address: destinationAddress,
+                  origin_latitude: userLatitude,
+                  origin_longitude: userLongitude,
+                  destination_latitude: destinationLatitude,
+                  destination_longitude: destinationLongitude,
+                  ride_time: rideTime.toFixed(0),
+                  fare_price: parseInt(amount) * 100,
+                  payment_status: "paid",
+                  driver_id: driverId,
+                  // user_id: userId,
+                }),
+              });
+
+              intentCreationCallback({
+                clientSecret: result.client_secret,
+              });
+            }
+          }
+        },
       },
+      returnURL: "myapp://book-ride",
     });
-    if (error) {
-      // handle error
+
+    if (!error) {
+      // setLoading(true);
     }
   };
 
   useEffect(() => {
     initializePaymentSheet();
   }, []);
-
-  const confirmHandler = async (
-    paymentMethod,
-    shouldSavePaymentMethod,
-    intentCreationCallback
-  ) => {
-    // explained later
-  };
-
-  //   const didTapCheckoutButton = async () => {
-  //     // implement later
-  //   };
 
   return (
     <>
